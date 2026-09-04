80ee3b3a82c1c36aa4e60aeb24f0d72aacb7e8e4
Jigil AK <jigil.ak2003@gmail.com>
2026-09-04 15:22:36 +0530

    Completed flight tool and tested

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
 from Tools.tavily_tool import tavily_search
+from Tools.flight_tool import search_flights
 
 
+#res = tavily_search("Best hotels in india")
+#print(res)
 
-res = tavily_search("Best hotels in india")
+
+res = search_flights("plan a 7 days Nepal trip from Bangladesh")
 print(res)
