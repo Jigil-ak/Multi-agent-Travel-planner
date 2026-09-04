38d972ea61c0cdc50e1af0b7ffc95b838cbe4a62
Jigil AK <jigil.ak2003@gmail.com>
2026-09-04 17:29:27 +0530

    Completd the agents and checked the working

--- test.py
+++ test.py
@@ -1,10 +1,21 @@
 from Tools.tavily_tool import tavily_search
 from Tools.flight_tool import search_flights
-
+from Backend import run_travel_agent
 
 #res = tavily_search("Best hotels in india")
 #print(res)
 
 
-res = search_flights("plan a 7 days Nepal trip from Bangladesh")
-print(res)
+#res = search_flights("plan a 7 days Nepal trip from Bangladesh")
+#print(res)
+
+
+user_input = input("Enter travel request: ")
+
+response = run_travel_agent(
+    user_input=user_input,
+    thread_id="test_user"
+)
+
+print("\nFINAL RESPONSE:\n")
+print(response["answer"])
